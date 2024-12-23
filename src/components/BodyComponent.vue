3cc578b27952aa20e94b7b5a4ae8c57ba09b902f
Aarón Morcillo Martínez <heyron53@gmail.com>
2024-12-23 10:30:45 +0100

    Added bootstrap library

--- src/components/BodyComponent.vue
+++ src/components/BodyComponent.vue
@@ -1,8 +1,16 @@
 <template>
-    <h1>Human Body</h1>
-    <div style="width: 25em">
-        <MaleFront/>
-        <MaleBack/>
+    <div class="d-flex justify-content-center">
+        <h1>Human Body</h1>
+    </div>
+    <div class="d-flex justify-content-center">
+        <div class="row" style="width: 50em">
+            <div class="col-6">
+                <MaleFront/>
+            </div>
+            <div class="col-6">
+                <MaleBack/>
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
