44847154f5c6eed3f37bbb232c69cb8528b2ce39
Aarón Morcillo Martínez <heyron53@gmail.com>
2024-12-23 11:37:45 +0100

    Added change body type button

--- src/components/BodyComponent.vue
+++ src/components/BodyComponent.vue
@@ -1,15 +1,22 @@
 <template>
     <div class="d-flex justify-content-center">
-        <h1>Human Body</h1>
+        <h1 v-if="!bodyChange">Male Human Body</h1>
+        <h1 v-else>Female Human Body</h1>
     </div>
     <div class="d-flex justify-content-center">
-        <div class="row" style="width: 50em">
+        <button type="button" class="btn btn-info" @click="changeBodyType">Change body type</button>
+    </div>
+    <div class="d-flex justify-content-center">
+        <div v-if="!bodyChange" class="row" style="width: 50em">
             <div class="col-6">
                 <MaleFront/>
             </div>
             <div class="col-6">
                 <MaleBack/>
             </div>
+            
+        </div>
+        <div v-else  class="row" style="width: 50em">
             <div class="col-6">
                 <FemaleFront/>
             </div>
@@ -18,7 +25,16 @@
             </div>
         </div>
     </div>
+    
 </template>
 <script setup>
+
+    import { ref } from 'vue';
+
+    const bodyChange = ref(false);
+
+    const changeBodyType = () => {
+        bodyChange.value = !bodyChange.value;
+    }
 
 </script>
